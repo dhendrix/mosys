6e91d6847f4520d717368237519bc0a6bc08d9d2
dhendrix@google.com <dhendrix@google.com@f72fe4e1-3c20-b1ed-aa54-d76f48875c1e>
2010-07-08 02:45:17 +0000

    Add sysfs scanning for SMBIOS table entries
    git-svn-id: https://mosys.googlecode.com/svn/trunk@44 f72fe4e1-3c20-b1ed-aa54-d76f48875c1e

--- lib/smbios/mosys_callbacks.c
+++ lib/smbios/mosys_callbacks.c
@@ -16,15 +16,71 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
+#include <errno.h>
+#include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "mosys/alloc.h"
+#include "mosys/list.h"
+#include "mosys/log.h"
 #include "mosys/platform.h"
 #include "mosys/string.h"
 
+#include "lib/file.h"
 #include "lib/smbios.h"
 #include "lib/smbios_tables.h"
+
+#define	SYSFS_SMBIOS_DIR	"/sys/class/dmi/id"
+
+/*
+ * smbios_scan_sysfs - scan for a SMBIOS entry by filename and return its value
+ *
+ * @filename:	filename to scan for
+ *
+ * Note: User must remember to free allocated memory
+ *
+ * returns pointer to allocated memory containing SMBIOS info
+ * returns NULL if not found
+ */
+static char *smbios_scan_sysfs(const char *filename)
+{
+	struct ll_node *list = NULL, *head;
+	int fd = 0;
+	char *val = NULL;
+	char *path;
+
+	list = scanft(&list, SYSFS_SMBIOS_DIR, filename, NULL, 1);
+	if (!list_count(list))
+		goto smbios_scan_sysfs_exit;
+
+	/* Note: this assumes 1 node in the list (1 SMBIOS table in sysfs) */
+	head = list_head(list);
+	path = (char *)head->data;
+
+	if ((fd = open(path, O_RDONLY)) < 0) {
+		lprintf(LOG_DEBUG, "Error opening %s: %s\n",
+		        path, strerror(errno));
+		goto smbios_scan_sysfs_exit;
+	}
+
+	val = mosys_malloc(SMBIOS_MAX_STRING_LENGTH);
+	memset(val, 0, SMBIOS_MAX_STRING_LENGTH);
+	if (read(fd, val, SMBIOS_MAX_STRING_LENGTH) < 0) {
+		lprintf(LOG_DEBUG, "Error reading %s: %s\n",
+		        path, strerror(errno));
+		free(val);
+		val = NULL;
+		goto smbios_scan_sysfs_exit;
+	}
+
+smbios_scan_sysfs_exit:
+	if (fd)
+		close(fd);
+
+	return val;
+}
 
 /*
  * smbios_sysinfo_get_vendor  -  return platform vendor
@@ -36,9 +92,19 @@
  */
 static char *smbios_sysinfo_get_vendor(struct platform_intf *intf)
 {
-	return smbios_find_string(intf, SMBIOS_TYPE_SYSTEM, 0,
-	                          SMBIOS_LEGACY_ENTRY_BASE,
-	                          SMBIOS_LEGACY_ENTRY_LEN);
+	char *str = NULL;
+
+	/* try normal SMBIOS approach first */
+	str = smbios_find_string(intf, SMBIOS_TYPE_SYSTEM, 0,
+	                         SMBIOS_LEGACY_ENTRY_BASE,
+	                         SMBIOS_LEGACY_ENTRY_LEN);
+
+	if (!str) {
+		lprintf(LOG_DEBUG, "%s: normal method failed, trying sysfs\n");
+		str = smbios_scan_sysfs("sys_vendor");
+	}
+
+	return str;
 }
 
 /*
@@ -51,9 +117,18 @@ static char *smbios_sysinfo_get_vendor(struct platform_intf *intf)
  */
 static char *smbios_sysinfo_get_name(struct platform_intf *intf)
 {
-	return smbios_find_string(intf, SMBIOS_TYPE_SYSTEM, 1,
-	                          SMBIOS_LEGACY_ENTRY_BASE,
-	                          SMBIOS_LEGACY_ENTRY_LEN);
+	char *str = NULL;
+
+	str = smbios_find_string(intf, SMBIOS_TYPE_SYSTEM, 1,
+	                         SMBIOS_LEGACY_ENTRY_BASE,
+	                         SMBIOS_LEGACY_ENTRY_LEN);
+
+	if (!str) {
+		lprintf(LOG_DEBUG, "%s: attempting to use sysfs\n");
+		str = smbios_scan_sysfs("product_name");
+	}
+
+	return str;
 }
 
 /*
@@ -66,9 +141,18 @@ static char *smbios_sysinfo_get_name(struct platform_intf *intf)
  */
 static char *smbios_sysinfo_get_version(struct platform_intf *intf)
 {
-	return smbios_find_string(intf, SMBIOS_TYPE_SYSTEM, 2,
-	                          SMBIOS_LEGACY_ENTRY_BASE,
-	                          SMBIOS_LEGACY_ENTRY_LEN);
+	char *str = NULL;
+
+	str = smbios_find_string(intf, SMBIOS_TYPE_SYSTEM, 2,
+	                         SMBIOS_LEGACY_ENTRY_BASE,
+	                         SMBIOS_LEGACY_ENTRY_LEN);
+
+	if (!str) {
+		lprintf(LOG_INFO, "%s: normal approach failed, trying sysfs\n");
+		str = smbios_scan_sysfs("product_version");
+	}
+
+	return str;
 }
 
 /*

1d6471d6c1abc23369358d1b79799d3bf54d1b55
dhendrix@google.com <dhendrix@google.com@f72fe4e1-3c20-b1ed-aa54-d76f48875c1e>
2010-07-23 01:13:56 +0000

    Add a missing #include for NM10-related patch (r62)
    git-svn-id: https://mosys.googlecode.com/svn/trunk@63 f72fe4e1-3c20-b1ed-aa54-d76f48875c1e

---
+++ include/drivers/intel/ich_generic.h
@@ -0,0 +1,30 @@
+/*                                                                                                           
+ * Copyright (C) 2010 Google Inc.                                                                            
+ *                                                                                                           
+ * This program is free software; you can redistribute it and/or                                             
+ * modify it under the terms of the GNU General Public License                                               
+ * as published by the Free Software Foundation; either version 2                                            
+ * of the License, or (at your option) any later version.                                                    
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#ifndef MOSYS_DRIVERS_INTEL_ICH_H__
+#define MOSYS_DRIVERS_INTEL_ICH_H__
+
+enum ich_bbs {
+       ICH_BBS_UNKNOWN = -1,
+       ICH_BBS_RSVD    = 0x0,
+       ICH_BBS_SPI     = 0x1,
+       ICH_BBS_PCI     = 0x2,
+       ICH_BBS_LPC     = 0x3,
+};
+
+#endif /* MOSYS_DRIVERS_INTEL_ICH_H__ */

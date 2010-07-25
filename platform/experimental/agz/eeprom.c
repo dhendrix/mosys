c5adbae4075c4de42aa9d8452e496c4f1087a7e9
dhendrix@google.com <dhendrix@google.com@f72fe4e1-3c20-b1ed-aa54-d76f48875c1e>
2010-07-25 08:08:33 +0000

    Add a "get_map" method for AGZ platform to retrieve a copy of FMAP data struct in EEPROM.
    Tested on AGZ w/ FMAP injected into host firmware image.
    
    TODO: This can probably be generalized for other platforms.
    
    
    
    git-svn-id: https://mosys.googlecode.com/svn/trunk@79 f72fe4e1-3c20-b1ed-aa54-d76f48875c1e

--- platform/experimental/agz/eeprom.c
+++ platform/experimental/agz/eeprom.c
@@ -19,11 +19,13 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#include "mosys/alloc.h"
 #include "mosys/platform.h"
 #include "mosys/log.h"
 
 #include "lib/eeprom.h"
 #include "lib/eeprom_enet.h"
+#include "lib/fmap.h"
 #include "lib/smbios.h"
 
 #include "drivers/intel/nm10.h"
@@ -101,9 +103,52 @@ static int agz_host_firmware_read(struct platform_intf *intf,
 	return 0;
 }
 
+/*
+ * agz_get_host_fmap - return a fmap structure for host firmware
+ *
+ * @intf:	platform interface
+ * @eeprom:	eeprom structure
+ *
+ * returns a newly allocated struct fmap if successful
+ * returns NULL to indicate error
+ */
+static struct fmap *agz_get_host_fmap(struct platform_intf *intf,
+                                      struct eeprom *eeprom)
+{
+	uint8_t *buf;
+	struct fmap *fmap_embedded, *fmap;
+	int fmap_offset, fmap_size;
+	int len;
+
+	if ((len= eeprom->device->size(intf, eeprom)) < 0)
+		return NULL;
+
+	buf = mosys_malloc(len);
+
+	if (eeprom->device->read(intf, eeprom, 0, (unsigned int)len, buf) < 0) {
+		lprintf(LOG_DEBUG, "%s: failed to read device\n", __func__);
+		return NULL;
+	}
+
+	if ((fmap_offset = fmap_find(buf, len)) < 0) {
+		lprintf(LOG_DEBUG, "%s: cannot find fmap\n", __func__);
+		return NULL;
+	}
+
+	fmap_embedded = (struct fmap *)(buf + fmap_offset);
+	fmap_size = sizeof(*fmap) + (fmap_embedded->nareas *
+	                             sizeof(fmap_embedded->areas[0]));
+	fmap = malloc(fmap_size);
+	memcpy(fmap, fmap_embedded, fmap_size);
+
+	free(buf);
+	return fmap;
+}
+
 static struct eeprom_dev agz_host_firmware = {
 	.size		= agz_host_firmware_size,
 	.read		= agz_host_firmware_read,
+	.get_map	= agz_get_host_fmap,
 };
 
 static struct eeprom agz_pinetrail_eeproms[] = {

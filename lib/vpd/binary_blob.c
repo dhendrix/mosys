a3842018a81454e13dd2e04fd72cb36b1e59aeed
dhendrix@google.com <dhendrix@google.com@f72fe4e1-3c20-b1ed-aa54-d76f48875c1e>
2010-08-18 07:29:15 +0000

    AGZ platform binary blob decoder
    git-svn-id: https://mosys.googlecode.com/svn/trunk@129 f72fe4e1-3c20-b1ed-aa54-d76f48875c1e

--- lib/vpd/binary_blob.c
+++ lib/vpd/binary_blob.c
@@ -30,9 +30,57 @@
  * binary_blob.c: helper functions for handling binary blobs
  */
 
+#include <stdlib.h>
 #include <uuid/uuid.h>
 
 #include "mosys/log.h"
 
+#include "lib/string.h"
 #include "lib/vpd_tables.h"
 #include "lib/vpd_binary_blob.h"
+
+int print_agz_blob(uint8_t *data, uint32_t size, struct kv_pair *kv)
+{
+	struct agz_blob_0_3 *agz_blob = data;
+	char s[37];
+	char *tmpstr;
+	int i;
+
+	if (size != sizeof(*agz_blob)) {
+		lprintf(LOG_DEBUG, "AGZ binary blob expected size: %lu, "
+		                   "got: %lu\n", sizeof(*agz_blob), size);
+		return -1;
+	}
+
+	kv_pair_add(kv, "blob_type", "agz_blob");
+
+	snprintf(s, sizeof(agz_blob->product_name) + 1, "%s", agz_blob->product_name);
+	kv_pair_add(kv, "product_name", s);
+
+	snprintf(s, sizeof(agz_blob->product_manufacturer) + 1,
+	         "%s", agz_blob->product_manufacturer);
+	kv_pair_add(kv, "product_manufacturer", s);
+
+	uuid_unparse(agz_blob->uuid, s);
+	kv_pair_add(kv, "product_uuid", s);
+
+	tmpstr = buf2str(agz_blob->motherboard_serial_number,
+	                 sizeof(agz_blob->motherboard_serial_number));
+	kv_pair_add(kv, "motherboard_serial_number", tmpstr);
+	free(tmpstr);
+
+	tmpstr = buf2str(agz_blob->esn_3g,
+	                 sizeof(agz_blob->esn_3g));
+	kv_pair_add(kv, "3g_esn", tmpstr);
+	free(tmpstr);
+
+	snprintf(s, sizeof(agz_blob->country_code) + 1,
+	         "%s", agz_blob->country_code);
+	kv_pair_add(kv, "country_code", s);
+
+	snprintf(s, sizeof( agz_blob->product_serial_number) + 1,
+	         "%s", agz_blob->product_serial_number);
+	kv_pair_fmt(kv, "product_serial_number", s);
+
+	return 0;
+}

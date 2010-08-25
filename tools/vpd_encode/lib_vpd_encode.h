cf150ef46d024a57244a03dec2aa6ccb63c51f81
dhendrix@google.com <dhendrix@google.com@f72fe4e1-3c20-b1ed-aa54-d76f48875c1e>
2010-08-25 20:26:30 +0000

    tidy up some copyright headers
    git-svn-id: https://mosys.googlecode.com/svn/trunk@145 f72fe4e1-3c20-b1ed-aa54-d76f48875c1e

--- tools/vpd_encode/lib_vpd_encode.h
+++ tools/vpd_encode/lib_vpd_encode.h
@@ -14,7 +14,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
- *
  */
 
 #ifndef VPD_ENCODE_VENDOR_BLOBS_H__

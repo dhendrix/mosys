bf64ddf0e52a1128c4217dde7b445006687d1894
dhendrix@google.com <dhendrix@google.com@f72fe4e1-3c20-b1ed-aa54-d76f48875c1e>
2011-02-09 02:51:17 +0000

    add support for "memory spd print id" for Mario
    git-svn-id: https://mosys.googlecode.com/svn/trunk@264 f72fe4e1-3c20-b1ed-aa54-d76f48875c1e

--- platform/experimental/mario/memory.c
+++ platform/experimental/mario/memory.c
@@ -16,7 +16,10 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
+#include "mosys/log.h"
 #include "mosys/platform.h"
+
+#include "lib/spd.h"
 
 #define MARIO_DIMM_COUNT	1
 
@@ -32,6 +35,68 @@ static int mario_dimm_count(struct platform_intf *intf)
 	return MARIO_DIMM_COUNT;
 }
 
+/*
+ * dimm_map  -  Convert logical dimm number to useful values
+ *
+ * @intf:       platform interface
+ * @dimm:       logical dimm number
+ * @type:       conversion type
+ *
+ * returns specified converted value
+ * returns <0 to indicate error
+ */
+static int mario_dimm_map(struct platform_intf *intf,
+                          enum dimm_map_type type, int dimm)
+{
+	int ret = -1;
+	static struct dimm_map {
+		int node;
+		int channel;
+		int slot;
+		int bus;
+		int address;
+	} mario_dimm_map[MARIO_DIMM_COUNT] = {
+		/* Node 0 */
+		{ 0, 0, 0, 2, 0x50 }
+	};
+
+	if (dimm < 0 || dimm >= intf->cb->memory->dimm_count(intf)) {
+		lprintf(LOG_ERR, "Invalid DIMM: %d\n", dimm);
+		return -1;
+	}
+
+	switch (type) {
+	case DIMM_TO_BUS:
+		ret = mario_dimm_map[dimm].bus;
+		break;
+	case DIMM_TO_ADDRESS:
+		ret = mario_dimm_map[dimm].address;
+		break;
+	default:
+		break;
+	}
+
+	return ret;
+}
+
+static int mario_spd_read(struct platform_intf *intf,
+                          int dimm, int reg, int len, uint8_t *buf)
+{
+	int bus;
+	int address;
+
+	bus = intf->cb->memory->dimm_map(intf, DIMM_TO_BUS, dimm);
+	address = intf->cb->memory->dimm_map(intf, DIMM_TO_ADDRESS, dimm);
+
+	return spd_read_i2c(intf, bus, address, reg, len, buf);
+}
+
+static struct memory_spd_cb mario_spd_cb = {
+	.read		= mario_spd_read,
+};
+
 struct memory_cb mario_pinetrail_memory_cb = {
-	.dimm_count		= mario_dimm_count,
+	.dimm_count	= mario_dimm_count,
+	.dimm_map	= mario_dimm_map,
+	.spd		= &mario_spd_cb,
 };

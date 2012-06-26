dbc5a8fe91f9d0e0917d369be276e3f93e26b2ac
Duncan Laurie <dlaurie@chromium.org>
2012-06-26 11:16:18 -0700

    Add ELOG support to the SMBIOS eventlog library
    This was ported from gsys and modified to support the
    actual implementation and event types in coreboot ELOG.
    
    BUG=chrome-os-partner:9195
    TEST=adds new unused infrastructure, used in later commits.
    
    Change-Id: I055fdf60b3a541219ffb1df77c7297b94d9837a9
    Signed-off-by: Duncan Laurie <dlaurie@chromium.org>

---
+++ include/lib/elog.h
@@ -0,0 +1,236 @@
+/*
+ * Copyright 2012, Google Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials provided
+ *      with the distribution.
+ *    * Neither the name of Google Inc. nor the names of its
+ *      contributors may be used to endorse or promote products derived
+ *      from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef MOSYS_LIB_ELOG_H_
+#define MOSYS_LIB_ELOG_H_
+
+#include <inttypes.h>
+
+struct platform_intf;
+struct kv_pair;
+struct smbios_log_entry;
+struct smbios_table_log;
+
+#define ELOG_HEADER_FORMAT	0x88
+#define ELOG_MAGIC		0x474f4c45 /* 'ELOG' */
+#define ELOG_VERSION		1
+
+struct elog_header {
+	uint32_t elog_magic;
+	uint8_t elog_version;
+	uint8_t elog_size;
+	uint8_t reserved[2];
+} __attribute__ ((packed));
+
+extern int elog_print_type(struct platform_intf *intf,
+                           struct smbios_log_entry *entry, struct kv_pair *kv);
+extern int elog_print_data(struct platform_intf *intf,
+                           struct smbios_log_entry *entry, struct kv_pair *kv);
+extern int elog_verify(struct platform_intf *intf,
+                       struct smbios_log_entry *entry);
+extern int elog_verify_metadata(struct smbios_table_log *table,
+                                void *eventlog_header);
+
+/*
+ * Generic event log payloads modified by Google
+ */
+struct elog_clear_event {
+	uint16_t bytes;
+} __attribute__((packed));
+
+/*
+ * Event log events
+ *
+ * Note that for events that we write to elog from gsys, we also define a
+ * _no_checksum type to send through the SMI interface.
+ */
+
+#define ELOG_TYPE_OS_EVENT                0x81
+struct elog_os_event {
+	uint8_t reason;
+	uint8_t checksum;
+} __attribute__ ((packed));
+# define ELOG_OS_EVENT_CLEAN              0  /* Clean Shutdown */
+# define ELOG_OS_EVENT_NMIWDT             1  /* NMI Watchdog */
+# define ELOG_OS_EVENT_PANIC              2  /* Panic */
+# define ELOG_OS_EVENT_OOPS               3  /* Oops */
+# define ELOG_OS_EVENT_DIE                4  /* Die */
+# define ELOG_OS_EVENT_MCE                5  /* MCE */
+# define ELOG_OS_EVENT_SOFTWDT            6  /* Software Watchdog */
+# define ELOG_OS_EVENT_MBE                7  /* MBE */
+# define ELOG_OS_EVENT_TRIPLE             8  /* Triple Fault */
+
+#define ELOG_TYPE_OS_BOOT                 0x90
+struct elog_os_boot {
+	uint32_t boot_count;
+	uint8_t checksum;
+} __attribute__ ((packed));
+
+#define ELOG_TYPE_EC_EVENT                0x91
+#define EC_EVENT_LID_CLOSED                0x01
+#define EC_EVENT_LID_OPEN                  0x02
+#define EC_EVENT_POWER_BUTTON              0x03
+#define EC_EVENT_AC_CONNECTED              0x04
+#define EC_EVENT_AC_DISCONNECTED           0x05
+#define EC_EVENT_BATTERY_LOW               0x06
+#define EC_EVENT_BATTERY_CRITICAL          0x07
+#define EC_EVENT_BATTERY                   0x08
+#define EC_EVENT_THERMAL_THRESHOLD         0x09
+#define EC_EVENT_THERMAL_OVERLOAD          0x0a
+#define EC_EVENT_THERMAL                   0x0b
+#define EC_EVENT_USB_CHARGER               0x0c
+#define EC_EVENT_KEY_PRESSED               0x0d
+struct elog_ec_event {
+	uint8_t event;
+	uint8_t checksum;
+} __attribute__ ((packed));
+
+/* Power */
+#define ELOG_TYPE_POWER_FAIL              0x92
+#define ELOG_TYPE_SUS_POWER_FAIL          0x93
+#define ELOG_TYPE_PWROK_FAIL              0x94
+#define ELOG_TYPE_SYS_PWROK_FAIL          0x95
+#define ELOG_TYPE_POWER_ON                0x96
+#define ELOG_TYPE_POWER_BUTTON            0x97
+#define ELOG_TYPE_POWER_BUTTON_OVERRIDE   0x98
+
+/* Reset */
+#define ELOG_TYPE_RESET_BUTTON            0x99
+#define ELOG_TYPE_SYSTEM_RESET            0x9a
+#define ELOG_TYPE_RTC_RESET               0x9b
+#define ELOG_TYPE_TCO_RESET               0x9c
+
+/* Sleep/Wake */
+#define ELOG_TYPE_ACPI_ENTER              0x9d
+#define ELOG_TYPE_ACPI_WAKE               0x9e
+#define ELOG_TYPE_WAKE_SOURCE             0x9f
+#define ELOG_WAKE_SOURCE_PCIE              0x00
+#define ELOG_WAKE_SOURCE_PME               0x01
+#define ELOG_WAKE_SOURCE_PME_INTERNAL      0x02
+#define ELOG_WAKE_SOURCE_RTC               0x03
+#define ELOG_WAKE_SOURCE_GPIO              0x04
+#define ELOG_WAKE_SOURCE_SMBUS             0x05
+struct elog_wake_source {
+	uint8_t source;
+	uint32_t instance;
+	uint8_t checksum;
+} __attribute__ ((packed));
+
+/* Chrome OS related Events */
+#define ELOG_TYPE_CROS_DEVELOPER_MODE    0xa0
+#define ELOG_TYPE_CROS_RECOVERY_MODE     0xa1
+struct elog_cros_recovery_mode {
+	uint8_t reason;
+	uint8_t checksum;
+} __attribute__ ((packed));
+
+/* Recovery reason codes for EVENT_TYPE_CROS_RECOVERY_MODE */
+/* Recovery not requested. */
+#define VBNV_RECOVERY_NOT_REQUESTED   0x00
+/* Recovery requested from legacy utility.  (Prior to the NV storage
+ * spec, recovery mode was a single bitfield; this value is reserved
+ * so that scripts which wrote 1 to the recovery field are
+ * distinguishable from scripts whch use the recovery reasons listed
+ * here. */
+#define VBNV_RECOVERY_LEGACY          0x01
+/* User manually requested recovery via recovery button */
+#define VBNV_RECOVERY_RO_MANUAL       0x02
+/* RW firmware failed signature check (neither RW firmware slot was valid) */
+#define VBNV_RECOVERY_RO_INVALID_RW   0x03
+/* S3 resume failed */
+#define VBNV_RECOVERY_RO_S3_RESUME    0x04
+/* TPM error in read-only firmware */
+#define VBNV_RECOVERY_RO_TPM_ERROR    0x05
+/* Shared data error in read-only firmware */
+#define VBNV_RECOVERY_RO_SHARED_DATA  0x06
+/* Test error from S3Resume() */
+#define VBNV_RECOVERY_RO_TEST_S3      0x07
+/* Test error from LoadFirmwareSetup() */
+#define VBNV_RECOVERY_RO_TEST_LFS     0x08
+/* Test error from LoadFirmware() */
+#define VBNV_RECOVERY_RO_TEST_LF      0x09
+/* RW firmware failed signature check (neither RW firmware slot was valid).
+ * Recovery reason is VBNV_RECOVERY_RO_INVALID_RW_CHECK_MIN + the check value
+ * for the slot which came closest to validating; see VBSD_LF_CHECK_* in
+ * vboot_struct.h. */
+#define VBNV_RECOVERY_RO_INVALID_RW_CHECK_0  0x10
+#define VBNV_RECOVERY_RO_INVALID_RW_CHECK_1  0x11
+#define VBNV_RECOVERY_RO_INVALID_RW_CHECK_2  0x12
+#define VBNV_RECOVERY_RO_INVALID_RW_CHECK_3  0x13
+#define VBNV_RECOVERY_RO_INVALID_RW_CHECK_4  0x14
+#define VBNV_RECOVERY_RO_INVALID_RW_CHECK_5  0x15
+#define VBNV_RECOVERY_RO_INVALID_RW_CHECK_6  0x16
+#define VBNV_RECOVERY_RO_INVALID_RW_CHECK_7  0x17
+#define VBNV_RECOVERY_RO_INVALID_RW_CHECK_8  0x18
+#define VBNV_RECOVERY_RO_INVALID_RW_CHECK_9  0x19
+#define VBNV_RECOVERY_RO_INVALID_RW_CHECK_A  0x1A
+#define VBNV_RECOVERY_RO_INVALID_RW_CHECK_B  0x1B
+#define VBNV_RECOVERY_RO_INVALID_RW_CHECK_C  0x1C
+#define VBNV_RECOVERY_RO_INVALID_RW_CHECK_D  0x1D
+#define VBNV_RECOVERY_RO_INVALID_RW_CHECK_E  0x1E
+#define VBNV_RECOVERY_RO_INVALID_RW_CHECK_F  0x1F
+/* Firmware boot failure outside of verified boot (RAM init, missing SSD,
+ * etc.). */
+#define VBNV_RECOVERY_RO_FIRMWARE     0x20
+/* Recovery mode TPM initialization requires a system reboot.  The system was
+ * already in recovery mode for some other reason when this happened. */
+#define VBNV_RECOVERY_RO_TPM_REBOOT   0x21
+/* Unspecified/unknown error in read-only firmware */
+#define VBNV_RECOVERY_RO_UNSPECIFIED  0x3F
+/* User manually requested recovery by pressing a key at developer
+ * warning screen */
+#define VBNV_RECOVERY_RW_DEV_SCREEN   0x41
+/* No OS kernel detected */
+#define VBNV_RECOVERY_RW_NO_OS        0x42
+/* OS kernel failed signature check */
+#define VBNV_RECOVERY_RW_INVALID_OS   0x43
+/* TPM error in rewritable firmware */
+#define VBNV_RECOVERY_RW_TPM_ERROR    0x44
+/* RW firmware in dev mode, but dev switch is off */
+#define VBNV_RECOVERY_RW_DEV_MISMATCH 0x45
+/* Shared data error in rewritable firmware */
+#define VBNV_RECOVERY_RW_SHARED_DATA  0x46
+/* Test error from LoadKernel() */
+#define VBNV_RECOVERY_RW_TEST_LK      0x47
+/* No bootable disk found */
+#define VBNV_RECOVERY_RW_NO_DISK      0x48
+/* Unspecified/unknown error in rewritable firmware */
+#define VBNV_RECOVERY_RW_UNSPECIFIED  0x7F
+/* DM-verity error */
+#define VBNV_RECOVERY_KE_DM_VERITY    0x81
+/* Unspecified/unknown error in kernel */
+#define VBNV_RECOVERY_KE_UNSPECIFIED  0xBF
+/* Recovery mode test from user-mode */
+#define VBNV_RECOVERY_US_TEST         0xC1
+/* Unspecified/unknown error in user-mode */
+#define VBNV_RECOVERY_US_UNSPECIFIED  0xFF
+
+#endif /* MOSYS_LIB_ELOG_H_ */
---
+++ lib/smbios/elog.c
@@ -0,0 +1,315 @@
+/*
+ * Copyright 2012, Google Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials provided
+ *      with the distribution.
+ *    * Neither the name of Google Inc. nor the names of its
+ *      contributors may be used to endorse or promote products derived
+ *      from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#include <stdlib.h>
+#include <inttypes.h>
+#include <valstr.h>
+
+#include "lib/elog.h"
+#include "lib/smbios.h"
+#include "lib/string.h"
+
+#include "mosys/kv_pair.h"
+#include "mosys/log.h"
+#include "mosys/platform.h"
+
+/*
+ * elog_verify_header - verify and validate if header is a valid Google Event
+ *                      Log header.
+ *
+ * @header:  pointer to elog header to verify
+ *
+ * return < 0 if invalid, 0 if valid
+ */
+static int elog_verify_header(void *header)
+{
+	struct elog_header *hdr;
+
+	if (header == NULL)
+		return -1;
+
+	hdr = header;
+
+	if (hdr->elog_magic != ELOG_MAGIC)
+		return -1;
+	if (hdr->elog_size != sizeof(*hdr))
+		return -1;
+	if (hdr->elog_version != ELOG_VERSION)
+		return -1;
+
+	return 0;
+}
+
+int elog_verify_metadata(struct smbios_table_log *table, void *eventlog_header)
+{
+	MOSYS_DCHECK(table);
+
+	if (table->header_format != ELOG_HEADER_FORMAT)
+		return -1;
+	if (elog_verify_header(eventlog_header) < 0)
+		return -1;
+
+	return 0;
+}
+
+/*
+ * elog_print_type - add the type of the entry to the kv_pair
+ *
+ * @intf:   platform interface used for low level hardware access
+ * @entry:  the smbios log entry to get type information
+ * @kv:     kv_pair structure to add type information to
+ *
+ * Returns 0 on failure, 1 on success.
+ */
+int elog_print_type(struct platform_intf *intf, struct smbios_log_entry *entry,
+                    struct kv_pair *kv)
+{
+	const char *type;
+	const struct valstr elog_event_types[] = {
+		{ ELOG_TYPE_OS_EVENT, "Kernel Event" },
+		{ ELOG_TYPE_OS_BOOT, "OS Boot" },
+		{ ELOG_TYPE_EC_EVENT, "EC Event" },
+		{ ELOG_TYPE_POWER_FAIL, "Power Fail" },
+		{ ELOG_TYPE_SUS_POWER_FAIL, "SUS Power Fail" },
+		{ ELOG_TYPE_PWROK_FAIL, "PWROK Fail" },
+		{ ELOG_TYPE_SYS_PWROK_FAIL, "SYS PWROK Fail" },
+		{ ELOG_TYPE_POWER_ON, "Power On" },
+		{ ELOG_TYPE_POWER_BUTTON, "Power Button" },
+		{ ELOG_TYPE_POWER_BUTTON_OVERRIDE, "Power Button Override" },
+		{ ELOG_TYPE_RESET_BUTTON, "Reset Button" },
+		{ ELOG_TYPE_SYSTEM_RESET, "System Reset" },
+		{ ELOG_TYPE_TCO_RESET, "RTC Reset" },
+		{ ELOG_TYPE_TCO_RESET, "TCO Reset" },
+		{ ELOG_TYPE_ACPI_ENTER, "ACPI Enter" },
+		{ ELOG_TYPE_ACPI_WAKE, "ACPI Wake" },
+		{ ELOG_TYPE_WAKE_SOURCE, "Wake Source" },
+		{ ELOG_TYPE_CROS_DEVELOPER_MODE, "Chrome OS Developer Mode" },
+		{ ELOG_TYPE_CROS_RECOVERY_MODE, "Chrome OS Recovery Mode" },
+		{ 0x0, NULL },
+	};
+
+	type = smbios_get_event_type_string(entry);
+
+	if (type == NULL) {
+		type = val2str_default(entry->type, elog_event_types, NULL);
+	}
+
+	if (type != NULL) {
+		kv_pair_add(kv, "type", type);
+		return 1;
+	}
+
+	lprintf(LOG_DEBUG, "elog: event type 0x%x unknown.\n", entry->type);
+
+	return 0;
+}
+
+/*
+ * elog_print_data - add the data associated with the entry to the kv_pair
+ *
+ * @intf:   platform interface used for low level hardware access
+ * @entry:  the smbios log entry to get the data information
+ * @kv:     kv_pair structure to add data to
+ *
+ * Returns 0 on failure, 1 on success.
+ */
+int elog_print_data(struct platform_intf *intf, struct smbios_log_entry *entry,
+                    struct kv_pair *kv)
+{
+	static struct valstr os_events[] = {
+		{ ELOG_OS_EVENT_CLEAN, "Clean Shutdown" },
+		{ ELOG_OS_EVENT_NMIWDT, "NMI Watchdog" },
+		{ ELOG_OS_EVENT_PANIC, "Panic" },
+		{ ELOG_OS_EVENT_OOPS, "Oops" },
+		{ ELOG_OS_EVENT_DIE, "Die" },
+		{ ELOG_OS_EVENT_MCE, "MCE" },
+		{ ELOG_OS_EVENT_SOFTWDT, "Software Watchdog" },
+		{ ELOG_OS_EVENT_MBE, "Multi-bit Error" },
+		{ ELOG_OS_EVENT_TRIPLE, "Triple Fault" },
+		{ 0, NULL },
+	};
+	static struct valstr wake_source_types[] = {
+		{ ELOG_WAKE_SOURCE_PCIE, "PCI Express" },
+		{ ELOG_WAKE_SOURCE_PME, "PCI PME" },
+		{ ELOG_WAKE_SOURCE_PME_INTERNAL, "Internal PME" },
+		{ ELOG_WAKE_SOURCE_RTC, "RTC Alarm" },
+		{ ELOG_WAKE_SOURCE_GPIO, "GPIO" },
+		{ ELOG_WAKE_SOURCE_SMBUS, "SMBALERT" },
+		{ 0, NULL },
+	};
+	static struct valstr ec_event_types[] = {
+		{ EC_EVENT_LID_CLOSED, "Lid Closed" },
+		{ EC_EVENT_LID_OPEN, "Lid Open" },
+		{ EC_EVENT_POWER_BUTTON, "Power Button" },
+		{ EC_EVENT_AC_CONNECTED, "AC Connected" },
+		{ EC_EVENT_AC_DISCONNECTED, "AC Disconnected" },
+		{ EC_EVENT_BATTERY_LOW, "Battery Low" },
+		{ EC_EVENT_BATTERY_CRITICAL, "Battery Critical" },
+		{ EC_EVENT_BATTERY, "Battery" },
+		{ EC_EVENT_THERMAL_THRESHOLD, "Thermal Threshold" },
+		{ EC_EVENT_THERMAL_OVERLOAD, "Thermal Overload" },
+		{ EC_EVENT_THERMAL, "Thermal" },
+		{ EC_EVENT_USB_CHARGER, "USB Charger" },
+		{ EC_EVENT_KEY_PRESSED, "Key Pressed" },
+		{ 0, NULL },
+	};
+	static struct valstr cros_recovery_reasons[] = {
+		{ VBNV_RECOVERY_LEGACY, "Legacy Utility" },
+		{ VBNV_RECOVERY_RO_MANUAL, "Recovery Button Pressed" },
+		{ VBNV_RECOVERY_RO_INVALID_RW, "RW Failed Signature Check" },
+		{ VBNV_RECOVERY_RO_S3_RESUME, "S3 Resume Failed" },
+		{ VBNV_RECOVERY_RO_TPM_ERROR, "TPM Error in RO Firmware" },
+		{ VBNV_RECOVERY_RO_SHARED_DATA,
+		  "Shared Data Error in RO Firmware" },
+		{ VBNV_RECOVERY_RO_TEST_S3, "Test Error from S3 Resume()" },
+		{ VBNV_RECOVERY_RO_TEST_LFS,
+		  "Test Error from LoadFirmwareSetup()" },
+		{ VBNV_RECOVERY_RO_TEST_LF,
+		  "Test Errofr from LoadFirmware()" },
+		{ VBNV_RECOVERY_RO_INVALID_RW_CHECK_0, "" },
+		{ VBNV_RECOVERY_RO_INVALID_RW_CHECK_1, "" },
+		{ VBNV_RECOVERY_RO_INVALID_RW_CHECK_2, "" },
+		{ VBNV_RECOVERY_RO_INVALID_RW_CHECK_3, "" },
+		{ VBNV_RECOVERY_RO_INVALID_RW_CHECK_4, "" },
+		{ VBNV_RECOVERY_RO_INVALID_RW_CHECK_5, "" },
+		{ VBNV_RECOVERY_RO_INVALID_RW_CHECK_6, "" },
+		{ VBNV_RECOVERY_RO_INVALID_RW_CHECK_7, "" },
+		{ VBNV_RECOVERY_RO_INVALID_RW_CHECK_8, "" },
+		{ VBNV_RECOVERY_RO_INVALID_RW_CHECK_9, "" },
+		{ VBNV_RECOVERY_RO_INVALID_RW_CHECK_A, "" },
+		{ VBNV_RECOVERY_RO_INVALID_RW_CHECK_B, "" },
+		{ VBNV_RECOVERY_RO_INVALID_RW_CHECK_C, "" },
+		{ VBNV_RECOVERY_RO_INVALID_RW_CHECK_D, "" },
+		{ VBNV_RECOVERY_RO_INVALID_RW_CHECK_E, "" },
+		{ VBNV_RECOVERY_RO_INVALID_RW_CHECK_F, "" },
+		{ VBNV_RECOVERY_RO_FIRMWARE, "Firmware Boot Failure" },
+		{ VBNV_RECOVERY_RO_TPM_REBOOT, "Recovery Mode TPM Reboot" },
+		{ VBNV_RECOVERY_RO_UNSPECIFIED,
+		  "Unknown Error in RO Firmware" },
+		{ VBNV_RECOVERY_RW_DEV_SCREEN,
+		  "User Requested from Developer Screen" },
+		{ VBNV_RECOVERY_RW_NO_OS, "No OS Kernel Detected" },
+		{ VBNV_RECOVERY_RW_INVALID_OS,
+		  "OS Kernel Failed Signature Check" },
+		{ VBNV_RECOVERY_RW_TPM_ERROR, "TPM Error in RW Firmware" },
+		{ VBNV_RECOVERY_RW_DEV_MISMATCH,
+		  "RW Dev Firmware but not Dev Mode" },
+		{ VBNV_RECOVERY_RW_SHARED_DATA,
+		  "Shared Data Error in RW Firmware" },
+		{ VBNV_RECOVERY_RW_TEST_LK, "Test Error from LoadKernel()" },
+		{ VBNV_RECOVERY_RW_NO_DISK, "No Bootable Disk Found" },
+		{ VBNV_RECOVERY_RW_UNSPECIFIED,
+		  "Unknown Error in RW Firmware" },
+		{ VBNV_RECOVERY_KE_DM_VERITY, "DM-Verity Error" },
+		{ VBNV_RECOVERY_KE_UNSPECIFIED, "Unknown Error in Kernel" },
+		{ VBNV_RECOVERY_US_TEST, "Test from User Mode" },
+		{ VBNV_RECOVERY_US_UNSPECIFIED, "Unknown Error in User Mode" },
+		{ 0, NULL },
+	};
+
+	switch (entry->type) {
+	case SMBIOS_EVENT_TYPE_LOGCLEAR:
+	{
+		uint16_t *bytes = (void *)&entry->data[0];
+		kv_pair_fmt(kv, "bytes", "%u", *bytes);
+		break;
+	}
+
+	case SMBIOS_EVENT_TYPE_BOOT:
+	{
+		uint32_t *count = (void *)&entry->data[0];
+		kv_pair_fmt(kv, "count", "%u", *count);
+		break;
+	}
+	case ELOG_TYPE_OS_EVENT:
+	{
+		uint32_t *event = (void *)&entry->data[0];
+		kv_pair_add(kv, "event", val2str(*event, os_events));
+		break;
+	}
+	case ELOG_TYPE_ACPI_ENTER:
+	case ELOG_TYPE_ACPI_WAKE:
+	{
+		uint8_t *state = (void *)&entry->data[0];
+		kv_pair_fmt(kv, "state", "S%u", *state);
+		break;
+	}
+	case ELOG_TYPE_WAKE_SOURCE:
+	{
+		struct elog_wake_source *event;
+		event = (void *)&entry->data[0];
+		kv_pair_add(kv, "source",
+			    val2str(event->source, wake_source_types));
+		kv_pair_fmt(kv, "instance", "%u", event->instance);
+		break;
+	}
+	case ELOG_TYPE_EC_EVENT:
+	{
+		uint8_t *event = (void *)&entry->data[0];
+		kv_pair_add(kv, "event", val2str(*event, ec_event_types));
+		break;
+	}
+	case ELOG_TYPE_CROS_RECOVERY_MODE:
+	{
+		uint8_t *reason = (void *)&entry->data[0];
+		kv_pair_add(kv, "reason",
+			    val2str(*reason, cros_recovery_reasons));
+		break;
+	}
+	default:
+		return 0;
+	}
+
+	return 0;
+}
+
+/*
+ * elog_verify - verify the contents of the SMBIOS log entry.
+ *
+ * @intf:   platform interface used for low level hardware access
+ * @entry:  the smbios log entry to verify
+ *
+ * returns 0 failure of verification, 1 if verification is sucessful
+ */
+int elog_verify(struct platform_intf *intf, struct smbios_log_entry *entry)
+{
+	char *data;
+	char checksum;
+	int i;
+
+	checksum = 0;
+	data = (void *)entry;
+
+	for (i = 0; i < entry->length; i++) {
+		checksum += data[i];
+	}
+
+	return checksum == 0;
+}

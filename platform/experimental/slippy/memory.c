af87174ae0012c006ebd97776d7bf781bb324993
Gene Chen <gene.chen@intel.com>
2013-10-24 17:22:41 +0800

    Leon: Add 2GB memory support in mosys
    Add 2GB memory support in mosys for probing HWID.
    
    BRANCH=none
    BUG=chrome-os-partner:23532
    TEST=execute "mosys -v -l memory spd print id"
    Check the output of the command having the deisred memory information.
    
    Change-Id: I0f560e9aee5020de633cc95f308d4fb0a973b67a
    Reviewed-on: https://chromium-review.googlesource.com/174482
    Reviewed-by: Mohammed Habibulla <moch@chromium.org>
    Tested-by: Gene Chen <gene.chen@intel.com>
    Commit-Queue: Gene Chen <gene.chen@intel.com>

--- platform/experimental/slippy/memory.c
+++ platform/experimental/slippy/memory.c
@@ -158,8 +158,17 @@ static int slippy_dimm_count(struct platform_intf *intf)
 	} else if (!strncmp(intf->name, "Samus", 5)) {
 		/* Samus has 2 DIMM config */
 		return 2;
+	} else if (!strncmp(intf->name, "Leon", 4)) {
+		/* Leon RAM_ID
+		 * {0,0,0} = 4G Micron
+		 * {0,0,1} = 4G Hynix
+		 * {0,1,0} = 4G Samsung
+		 * {1,0,1} = 2G Hynix
+		 * {1,1,0} = 2G Samsung
+		 */
+		return slippy_get_spd_index(intf) >= 4 ? 1 : 2;
 	}
-	/* FIXME: Add Leon and Wolf handling (if needed) */
+        /* FIXME: Add Wolf handling (if needed) */
 	else
 		return SLIPPY_DIMM_COUNT;
 }

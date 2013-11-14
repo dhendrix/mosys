811eb534200995d575aa79d29f33ff038b5f9d42
chuckche <chuck.chen@intel.com>
2013-11-14 14:38:32 +0800

    Wolf: Add 2GB memory support in mosys.
    Add 2GB memory support in mosys for probing HWID.
    BRANCH=master
    BUG=chrome-os-partner:23534
    TEST=execute "mosys -v -l memory spd print id"
    Check the output of the command having the deisred memory information.
    
    Change-Id: I1863bb1c1d7d51af05c74b341cf5a15c3655e1ce
    Signed-off-by: chuckche <chuck.chen@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/176780
    Reviewed-by: Mohammed Habibulla <moch@google.com>

--- platform/experimental/slippy/memory.c
+++ platform/experimental/slippy/memory.c
@@ -167,8 +167,17 @@ static int slippy_dimm_count(struct platform_intf *intf)
 		 * {1,1,0} = 2G Samsung
 		 */
 		return slippy_get_spd_index(intf) >= 4 ? 1 : 2;
+	} else if (!strncmp(intf->name, "Wolf", 4)) {
+		/* Wolf RAM_ID
+		 * {0,0,0} = 4G Micron
+		 * {0,0,1} = 4G Hynix
+		 * {0,1,0} = 4G Samsung
+		 * {0,1,1} = 2G Micron
+		 * {1,0,0} = 2G Hynix
+		 * {1,0,1} = 2G Samsung
+		 */
+		return slippy_get_spd_index(intf) >= 3 ? 1 : 2;
 	}
-        /* FIXME: Add Wolf handling (if needed) */
 	else
 		return SLIPPY_DIMM_COUNT;
 }

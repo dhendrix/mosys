273b0f3820ff58c0c686a6c7aa5ef6e8751581ef
David Hendricks <dhendrix@chromium.org>
2014-11-03 16:23:43 -0800

    pinky: Add memory command and callbacks
    This adds support for "mosys memory ..." commands for retrieving
    memory information.
    
    BUG=chrome-os-partner:31728
    BRANCH=none
    TEST="mosys memory" commands work on pinky w/ related firmware changes
    
    Signed-off-by: David Hendricks <dhendrix@chromium.org>
    Change-Id: Ib83e488549ffc8a136574e0e818c5410b3023b27
    Reviewed-on: https://chromium-review.googlesource.com/228877
    Reviewed-by: Julius Werner <jwerner@chromium.org>
    Commit-Queue: Gediminas Ramanauskas <gedis@chromium.org>

---
+++ platform/google/pinky/memory.c
@@ -0,0 +1,139 @@
+/*
+ * Copyright 2014, Google Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials provided
+ *      with the distribution.
+ *    * Neither the name of Google Inc. nor the names of its
+ *      contributors may be used to endorse or promote products derived
+ *      from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "lib/fdt.h"
+#include "lib/nonspd.h"
+
+#include "mosys/log.h"
+#include "mosys/platform.h"
+
+#include "pinky.h"
+
+const struct nonspd_mem_info samsung_8gbit_lpddr3 = {
+	.dram_type		= SPD_DRAM_TYPE_DDR3,
+	.module_type.ddr3_type	= DDR3_MODULE_TYPE_UNDEFINED,
+
+	.module_size_mbits	= 8192,
+	.num_ranks		= 2,
+	.device_width		= 32,
+	.ddr_freq 		= { DDR_533 },
+
+	.module_mfg_id		= { .msb = 0xce, .lsb = 0x00 },
+	.dram_mfg_id		= { .msb = 0xce, .lsb = 0x00 },
+
+	.serial_num 		= { 0, 0, 0, 0 },
+	.part_num		=
+		{ 'K', '4', 'E', '8', 'E', '3', '0', '4', 'E', 'D', '-',
+		  'E', 'G', 'C', 'C' },
+};
+
+const struct nonspd_mem_info samsung_4gbit_ddr3l = {
+	.dram_type		= SPD_DRAM_TYPE_DDR3,
+	.module_type.ddr3_type	= DDR3_MODULE_TYPE_UNDEFINED,
+
+	.module_size_mbits	= 4096,
+	.num_ranks		= 1,
+	.device_width		= 16,
+	.ddr_freq 		= { DDR_400, DDR_533, DDR_667, DDR_800 },
+
+	.module_mfg_id		= { .msb = 0xce, .lsb = 0x00 },
+	.dram_mfg_id		= { .msb = 0xce, .lsb = 0x00 },
+
+	.serial_num 		= { 0, 0, 0, 0 },
+	.part_num		=
+		{ 'K', '4', 'B', '4', 'G', '1', '6', '4', '6', 'D', '-',
+		  'B', 'Y', 'K', '0' },
+};
+
+static int pinky_dimm_count;
+static const struct nonspd_mem_info *pinky_mem_info;
+
+/* read RAM code and fill in values needed by memory commands */
+static int read_ram_code(struct platform_intf *intf)
+{
+	static int done = 0, ret = 0;
+	uint32_t ram_code;
+
+	if (done)
+		return ret;
+
+	ram_code = fdt_get_ram_code();
+	if (ram_code == ~(0)) {
+		lprintf(LOG_ERR, "Unable to obtain RAM code.\n");
+		return -1;
+	}
+
+	switch (ram_code) {
+	case 0:
+		pinky_dimm_count = 2;
+		pinky_mem_info = &samsung_8gbit_lpddr3;
+		break;
+	case 4:
+		pinky_dimm_count = 4;
+		pinky_mem_info = &samsung_4gbit_ddr3l;
+		break;
+	default:
+		ret = -1;
+		break;
+	}
+
+	return ret;
+}
+
+/*
+ * dimm_count  -  return total number of dimm slots
+ *
+ * @intf:       platform interface
+ *
+ * returns dimm slot count
+ */
+static int dimm_count(struct platform_intf *intf)
+{
+	if (read_ram_code(intf) < 0)
+		return -1;
+
+	return pinky_dimm_count;
+}
+
+static int get_mem_info(struct platform_intf *intf,
+			const struct nonspd_mem_info **info)
+{
+	if (read_ram_code(intf) < 0)
+		return -1;
+
+	*info = pinky_mem_info;
+	return 0;
+}
+
+struct memory_cb pinky_memory_cb = {
+	.dimm_count		= dimm_count,
+	.nonspd_mem_info	= &get_mem_info,
+};

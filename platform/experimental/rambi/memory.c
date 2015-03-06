c52f3626cf25c6aa26bb009fc97a0ee74a55fcd0
Katherine Hsieh <Katherine.Hsieh@quantatw.com>
2015-03-06 16:42:01 +0800

    Banjo: Modify SPD entries
    Based on SPD table to modify.
    
    BUG=None
    BRANCH=Banjo
    TEST=emerge-banjo mosys
    
    Change-Id: I8ad0d253511f68ec34832db89c2406edb7267224
    Signed-off-by: Katherine Hsieh <Katherine.Hsieh@quantatw.com>
    Reviewed-on: https://chromium-review.googlesource.com/256870
    Reviewed-by: Wesley Kuo <wesley.kuo@intel.com>
    Reviewed-by: T.H. Lin <T.H_Lin@quantatw.com>
    Tested-by: T.H. Lin <T.H_Lin@quantatw.com>
    Reviewed-by: Shawn N <shawnn@chromium.org>
    Commit-Queue: Wesley Kuo <wesley.kuo@intel.com>

--- platform/experimental/rambi/memory.c
+++ platform/experimental/rambi/memory.c
@@ -112,18 +112,16 @@ static int rambi_dimm_count(struct platform_intf *intf)
 	 */
 	if (!strncmp(intf->name, "Banjo", 5)) {
 		/*
-		* {0,0,0} = 2 x 2GiB Micron
+		* {0,0,0} = 1 x 2GiB Samsung
 		* {0,0,1} = 2 x 2GiB Hynix
-		* {0,1,0} = 2 x 1GiB Micron
-		* {0,1,1} = 1 x 2GiB Hynix
-		* {1,0,0} = 1 x 2GiB Micron
-		* {1,0,1} = 1 x 2GiB Hynix
-		* {1,1,0} = 2 x 2GiB Kingston
-		* {1,1,1} = 2 x 2GiB Hynix
+		* {0,1,0} = 1 x 2GiB Hynix
+		* {0,1,1} = 2 x 2GiB Samsung
+		* {1,0,0} = 1 x 2GiB Hynix
+		* {1,0,1} = 2 x 2GiB Hynix
 		*/
         int index = rambi_get_spd_index(intf);
         switch (index) {
-        case 3: case 4: case 5:
+        case 0: case 2: case 4:
             return 1;
         default:
             return 2;

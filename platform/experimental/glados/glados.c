d9df0a566bf16c8146a0b4b0c0e9025b7960d489
Ted Kuo <tedkuo@ami.com.tw>
2016-06-20 16:10:48 +0800

    asuka: add asuka board to the ID list
    Enable mosys support on asuka.
    
    BUG=None
    BRANCH=None
    TEST=successfully run mosys on asuka
    Signed-off-by: Ted Kuo <tedkuo@ami.com.tw>
    
    Change-Id: I859c7b7484ac6dfbe28092e58bfbf492a32d02b4
    Reviewed-on: https://chromium-review.googlesource.com/353882
    Commit-Ready: Ted Kuo <tedkuo@ami.com.tw>
    Tested-by: Ted Kuo <tedkuo@ami.com.tw>
    Reviewed-by: Shawn N <shawnn@chromium.org>

--- platform/experimental/glados/glados.c
+++ platform/experimental/glados/glados.c
@@ -53,6 +53,10 @@ struct probe_ids {
 };
 
 static const struct probe_ids probe_id_list[] = {
+	{ { "Asuka", NULL },
+	  { "ASUKA", NULL },
+	  { "Asuka", NULL },
+	},
 	{ { "Cave", NULL },
 	  { "CAVE", NULL },
 	  { "Cave", NULL },
